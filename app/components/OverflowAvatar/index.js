bd7e096aad17d786854aaf55279030723057e9e1
Greg Ziegan <greg.ziegan@gmail.com>
2016-05-25 19:18:25 -0700

    make overflow avatar a little prettier

--- app/components/OverflowAvatar/index.js
+++ app/components/OverflowAvatar/index.js
@@ -11,7 +11,7 @@ import styles from './styles.css'
 function OverflowAvatar(props) {
   return (
     <div className={styles.overflowAvatar}>
-      <Avatar>{`+${props.number}`}</Avatar>
+      <Avatar><span className={styles.plus}>+</span><span className={styles.number}>{props.number}</span></Avatar>
     </div>
   )
 }
