0fdcee95abcefc4923becbeb0386b35ff834b233
Greg Ziegan <greg.ziegan@gmail.com>
2016-05-28 23:57:02 +0100

    color those cast bubbles

--- app/components/Person/index.js
+++ app/components/Person/index.js
@@ -6,6 +6,8 @@
 
 import React from 'react'
 import Avatar from 'material-ui/Avatar'
+import { getColor } from 'random-material-color'
+
 import styles from './styles.css'
 
 function Person(props) {
@@ -13,7 +15,9 @@ function Person(props) {
     <div className={styles.person}>
       {props.avatar ?
         <Avatar src={props.avatar} /> :
-        <Avatar>{props.name[0]}</Avatar>
+        <Avatar
+          backgroundColor={getColor({ text: props.name })}
+        >{props.name[0]}</Avatar>
       }
     </div>
   )
